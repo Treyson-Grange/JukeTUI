800fc6ac82e7ec10172da8ffa17b74d856d769a0
Treyson Grange <treyson.grange@outlook.com>
2024-10-27 20:03:57 -0600

    model reduction

--- log.go
+++ log.go
@@ -5,6 +5,9 @@ import (
 	"os"
 )
 
+// log.go
+// This file contains all logging functionality.
+
 var errorLogger = func() *log.Logger {
 	file, err := os.OpenFile("errors.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
--- models.go
+++ models.go
@@ -101,82 +101,48 @@ type SpotifyRecommendations struct {
 }
 
 // PlaybackState struct for parsing the playback state response.
-// TODO: a lol of these are absolutely unused, and should be removed, so we don't have to store em.
 type PlaybackState struct {
 	Device struct {
-		ID            string `json:"id"`
-		IsActive      bool   `json:"is_active"`
-		IsRestricted  bool   `json:"is_restricted"`
-		Name          string `json:"name"`
-		Type          string `json:"type"`
-		VolumePercent int    `json:"volume_percent"`
+		ID string `json:"id"`
 	} `json:"device"`
 	ShuffleState bool   `json:"shuffle_state"`
 	RepeatState  string `json:"repeat_state"`
-	Timestamp    int64  `json:"timestamp"`
 	Context      struct {
-		ExternalUrls struct {
-			Spotify string `json:"spotify"`
-		} `json:"external_urls"`
-		Href string `json:"href"`
-		Type string `json:"type"`
-		URI  string `json:"uri"`
+		URI string `json:"uri"`
 	} `json:"context"`
 	ProgressMs int `json:"progress_ms"`
 	Item       struct {
 		Album struct {
 			AlbumType string `json:"album_type"`
 			Artists   []struct {
-				ExternalUrls struct {
-					Spotify string `json:"spotify"`
-				} `json:"external_urls"`
 				Href string `json:"href"`
 				ID   string `json:"id"`
 				Name string `json:"name"`
 				Type string `json:"type"`
 				URI  string `json:"uri"`
 			} `json:"artists"`
-			ExternalUrls struct {
-				Spotify string `json:"spotify"`
-			} `json:"external_urls"`
 			Href   string `json:"href"`
 			ID     string `json:"id"`
 			Images []struct {
-				Height int    `json:"height"`
-				URL    string `json:"url"`
-				Width  int    `json:"width"`
+				URL string `json:"url"`
 			} `json:"images"`
 			Name string `json:"name"`
 			Type string `json:"type"`
 			URI  string `json:"uri"`
 		} `json:"album"`
 		Artists []struct {
-			ExternalUrls struct {
-				Spotify string `json:"spotify"`
-			} `json:"external_urls"`
 			Href string `json:"href"`
 			ID   string `json:"id"`
 			Name string `json:"name"`
 			Type string `json:"type"`
 			URI  string `json:"uri"`
 		} `json:"artists"`
-		DiscNumber  int  `json:"disc_number"`
-		DurationMs  int  `json:"duration_ms"`
-		Explicit    bool `json:"explicit"`
-		ExternalIds struct {
-			Isrc string `json:"isrc"`
-		} `json:"external_ids"`
-		ExternalUrls struct {
-			Spotify string `json:"spotify"`
-		} `json:"external_urls"`
-		Href        string `json:"href"`
-		ID          string `json:"id"`
-		Name        string `json:"name"`
-		Popularity  int    `json:"popularity"`
-		PreviewURL  string `json:"preview_url"`
-		TrackNumber int    `json:"track_number"`
-		Type        string `json:"type"`
-		URI         string `json:"uri"`
+		DurationMs int    `json:"duration_ms"`
+		Href       string `json:"href"`
+		ID         string `json:"id"`
+		Name       string `json:"name"`
+		Type       string `json:"type"`
+		URI        string `json:"uri"`
 	} `json:"item"`
 	IsPlaying bool `json:"is_playing"`
 }
